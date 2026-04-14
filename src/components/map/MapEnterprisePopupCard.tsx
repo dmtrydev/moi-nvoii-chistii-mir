545fcf6ce7d5442085cc4ce179c91b279b46dfdc
Dmitry <dmtrydev@gmail.com>
2026-04-14 19:14:11 +0300

    Обновление

--- src/components/map/MapEnterprisePopupCard.tsx
+++ src/components/map/MapEnterprisePopupCard.tsx
@@ -24,10 +24,10 @@ export const MapEnterprisePopupCard = memo(function MapEnterprisePopupCard({ mod
   return (
     <article className="moinoviichistiimir-popup-enterprise">
       <header className="moinoviichistiimir-popup-enterprise__head">
-        <h4 className="moinoviichistiimir-popup-enterprise__title bg-[linear-gradient(136deg,rgba(43,51,53,1)_0%,rgba(97,110,114,1)_47%,rgba(43,51,53,1)_100%)] bg-clip-text text-transparent [-webkit-text-fill-color:transparent]">
+        <h3 className="typo-h3 moinoviichistiimir-popup-enterprise__title bg-[linear-gradient(136deg,rgba(43,51,53,1)_0%,rgba(97,110,114,1)_47%,rgba(43,51,53,1)_100%)] bg-clip-text text-transparent [-webkit-text-fill-color:transparent]">
           {model.title}
-        </h4>
-        <p className="moinoviichistiimir-popup-enterprise__address">{model.subtitleAddress}</p>
+        </h3>
+        <p className="typo-h6 moinoviichistiimir-popup-enterprise__address">{model.subtitleAddress}</p>
         <div className="moinoviichistiimir-popup-enterprise__headDivider" aria-hidden />
         <div className="moinoviichistiimir-popup-enterprise__icons" aria-label="Статусы предприятия">
           {statusIcons.map(({ id, iconSrc, active, label }) => (
@@ -51,8 +51,8 @@ export const MapEnterprisePopupCard = memo(function MapEnterprisePopupCard({ mod
         {model.infoRows.map((item, index) => (
           <div key={item.key} className="moinoviichistiimir-popup-enterprise__rowWrap">
             <div className="moinoviichistiimir-popup-enterprise__row">
-              <div className="moinoviichistiimir-popup-enterprise__label">{item.label}</div>
-              <div className="moinoviichistiimir-popup-enterprise__value">{item.value}</div>
+              <div className="typo-h5 moinoviichistiimir-popup-enterprise__label">{item.label}</div>
+              <div className="typo-h6 moinoviichistiimir-popup-enterprise__value">{item.value}</div>
             </div>
             {index < model.infoRows.length - 1 && <div className="moinoviichistiimir-popup-enterprise__divider" aria-hidden />}
           </div>
